b4678ce063ab5c152e6b8bb5af9394d4113b6f42
Emily Vavrinu <evavrinu@c1r6s5.42prague.com>
2026-05-13 17:53:17 +0200

    function that takes a pointer to an int as a parameter and sets the value of that int to "42".

---
+++ ex00/ft_ft.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evavrinu <evavrinu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/13 17:50:23 by evavrinu          #+#    #+#             */
+/*   Updated: 2026/05/13 17:52:54 by evavrinu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+// int	main(void)
+// {
+// 	int	number;
+// 	int	*ptr;
+
+// 	number = 0;
+// 	ptr = &number;
+// 	printf("Before: %d\n", number);
+// 	ft_ft(ptr);
+// 	printf("After: %d\n", number);
+// 	return (0);
+// }
